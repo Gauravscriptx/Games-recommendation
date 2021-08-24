0f48cb5be4d609f6cc218832d388759c465e4e98
Yogesh Yadav <70842648+Gauravscriptx@users.noreply.github.com>
2021-08-24 14:43:35 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,19 +1,30 @@
-import React from "react";
-import "./styles.css";
-import { useState } from "react";
+import React from 'react';
+import './styles.css';
+import { useState } from 'react';
 
 const gameDB = {
   Storyline: [
-    { name: "SimCity", rating: "4/5" },
-    { name: "Tamagotchi ", rating: "3.5/5" }
+    { name: 'SimCity', rating: '4/5' },
+    { name: 'Tamagotchi ', rating: '3.5/5' },
   ],
-   Action: [
+  Action: [
     {
-      name: "Donkey Kong",
-      rating: "5/5"
+      name: 'Donkey Kong',
+      rating: '5/5',
     },
     {
-      name: "Galaga",
-      rating: "4.5/5"
-    }
+      name: 'Galaga',
+      rating: '4.5/5',
+    },
   ],
+  Adventure: [
+    {
+      name: 'Dungeons & Dragons',
+      rating: '3.5/5',
+    },
+    {
+      name: 'Fallout',
+      rating: '5/5',
+    },
+  ],
+};
