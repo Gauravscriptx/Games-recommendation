2a4168db33ef1f949492932fa42fcda05e34ebac
Yogesh Yadav <70842648+Gauravscriptx@users.noreply.github.com>
2021-08-24 14:42:43 +0530

    Update app.js

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./styles.css";
+import { useState } from "react";
+
+const gameDB = {
+  Storyline: [
+    { name: "SimCity", rating: "4/5" },
+    { name: "Tamagotchi ", rating: "3.5/5" }
+  ],
